9b7f5a3ae205bbd51ecb2a05a2d0141c0d60c093
xiaowaibuzheng <782042369@qq.com>
2025-07-16 16:12:56 +0800

    refactor(chunk-split): 重构代码以提高可维护性和可测试性
    - 重新组织代码结构，增加可读性
    - 添加类型定义，提高代码健壮性
    - 优化 manualChunks 函数，使其更易于测试
    - 更新插件导出方式，采用默认导出

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,60 @@ import type { ChunkingContext } from 'rolldown-vite/types/internal/rollupTypeCom
 import type { Plugin } from 'vite'
 
 import { init } from 'es-module-lexer'
+import assert from 'node:assert'
 import path from 'node:path'
 
 import { staticImportedScan } from './staticImportScan'
 import { nodeName, normalizePath } from './utils'
 
-function wrapCustomSplitConfig(id: string, { getModuleInfo }: ChunkingContext) {
-  const cwd = process.cwd()
-  if (id.includes('node_modules')) {
-    if (staticImportedScan(id, getModuleInfo, new Map(), [])) {
-      return `p-${nodeName(id) ?? 'vender'
-      }`
-    }
-    else {
-      return `p-${nodeName(id) ?? 'vender'
-      }-async`
-    }
-  }
-  if (!id.includes('node_modules')) {
-    const extname = path.extname(id)
-    return normalizePath(path.relative(cwd, id).replace(extname, ''))
+type NullValue<T = void> = T | undefined | null | void;
+
+type ManualChunksOption = (id: string, context: ChunkingContext) => string | NullValue
+function wrapCustomSplitConfig(manualChunks: ManualChunksOption) {
+  assert(typeof manualChunks === 'function')
+  return (
+    moduleId: string,
+    { getModuleInfo }: ChunkingContext,
+  ) => {
+    return manualChunks(moduleId, { getModuleInfo })
   }
 }
-export function splitChunk(): Plugin {
+
+const cwd = process.cwd()
+function generateManualChunks() {
+  return wrapCustomSplitConfig(
+    (id: string, { getModuleInfo }: ChunkingContext): string | NullValue => {
+      if (id.includes('node_modules')) {
+        if (staticImportedScan(id, getModuleInfo, new Map(), [])) {
+          return `p-${nodeName(id) ?? 'vender'
+            }`
+        }
+        else {
+          return `p-${nodeName(id) ?? 'vender'
+            }-async`
+        }
+      }
+      if (!id.includes('node_modules')) {
+        const extname = path.extname(id)
+        return normalizePath(path.relative(cwd, id).replace(extname, ''))
+      }
+    },
+  )
+}
+
+export default (
+): Plugin => {
   return {
-    name: 'rolldown-vite-plugin-chunk-split',
+    name: 'vite-plugin-chunk-split',
     async config() {
       await init
+      const manualChunks = generateManualChunks()
       return {
         build: {
           rollupOptions: {
             output: {
               // TODO: 配置项 暂时先用 manualChunks
-              manualChunks: wrapCustomSplitConfig,
+              manualChunks: manualChunks,
               // advancedChunks: {
               //   groups: [
               //     {
